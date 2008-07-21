ed7dcc8fb4d1666be8962879dfe420b9076abf1c
jeremy <jeremy@n-heptane.com>
2008-07-21 20:17:28 +0000

    code formatting changes

--- src/HAppS/Server/Extra.hs
+++ src/HAppS/Server/Extra.hs
@@ -14,16 +14,16 @@ prettyRequest (Request method paths uri query inputs cookies version headers bod
           = thehtml ((thetitle (toHtml "404"))  +++
                      (body ((h1 (toHtml "Requested object not found.")) +++
                             (table
-                             ((tr (td (toHtml "method") +++ (td (toHtml (show method))))) +++
-                              (tr (td (toHtml "paths") +++ (td (toHtml (show paths))))) +++
-                              (tr (td (toHtml "uri") +++ (td (toHtml (show uri))))) +++
-                              (tr (td (toHtml "query") +++ (td (toHtml query)))) +++
-                              (tr (td (toHtml "inputs") +++ (td (toHtml (show inputs))))) +++
+                             ((tr (td (toHtml "method")  +++ (td (toHtml (show method)))))  +++
+                              (tr (td (toHtml "paths")   +++ (td (toHtml (show paths)))))   +++
+                              (tr (td (toHtml "uri")     +++ (td (toHtml (show uri)))))     +++
+                              (tr (td (toHtml "query")   +++ (td (toHtml query))))          +++
+                              (tr (td (toHtml "inputs")  +++ (td (toHtml (show inputs)))))  +++
                               (tr (td (toHtml "cookies") +++ (td (toHtml (show cookies))))) +++
                               (tr (td (toHtml "version") +++ (td (toHtml (show version))))) +++
                               (tr (td (toHtml "headers") +++ (td (toHtml (show headers))))) +++
-                              (tr (td (toHtml "peer") +++ (td (toHtml (show peer))))) +++
-                              (tr (td (toHtml "body") +++ (td (toHtml (show body')))))
+                              (tr (td (toHtml "peer")    +++ (td (toHtml (show peer)))))    +++
+                              (tr (td (toHtml "body")    +++ (td (toHtml (show body')))))
                              )
                             )
                            )
