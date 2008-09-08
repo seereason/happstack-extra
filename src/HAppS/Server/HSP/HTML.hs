1e772ed64037e8b81efd356e5a3bcb1df58f6f70
jeremy <jeremy@n-heptane.com>
2008-09-08 22:24:56 +0000

    ToMessage: set charset=utf-8 in Content-Type, since that is all we support anyway

--- src/HAppS/Server/HSP/HTML.hs
+++ src/HAppS/Server/HSP/HTML.hs
@@ -8,12 +8,12 @@ import HSP.XML (XML(..), XMLMetaData(..))
 import HSP.HTML (html4Strict, renderAsHTML)
 
 instance ToMessage XML where
-    toContentType _ = P.pack "text/html"
+    toContentType _ = P.pack "text/html;charset=utf-8"
     toMessage xml   = toMessage (html4Strict, xml)
 
 instance ToMessage (Maybe XMLMetaData, XML) where
     toContentType (Just md,_) = P.pack (contentType md)
-    toContentType _ = P.pack "text/html"
+    toContentType _ = P.pack "text/html;charset=utf-8"
     toMessage (Just (XMLMetaData (showDt, dt) _ pr), xml) = 
         L.fromString ((if showDt then (dt ++) else id) (pr xml))
     toMessage (Nothing, xml) =
