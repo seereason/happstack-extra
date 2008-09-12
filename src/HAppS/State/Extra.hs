9911825cfa700de26886ed365089865726316cbf
jeremy <jeremy@n-heptane.com>
2008-09-12 00:19:27 +0000

    add liftQuery/liftUpdate

--- src/HAppS/State/Extra.hs
+++ src/HAppS/State/Extra.hs
@@ -1,6 +1,7 @@
 module HAppS.State.Extra where
 
 import Control.Exception
+import Control.Monad.Trans(MonadIO(liftIO))
 import HAppS.State
 
 -- |execute 'action' with the state transaction system running
@@ -9,3 +10,12 @@ withSystemState :: (Component st, Methods st) => Proxy st -> IO a -> IO a
 withSystemState entryPoint action =
     do control <- startSystemState entryPoint
        action `finally` shutdownSystem control
+
+
+-- * these need better names
+
+liftQuery :: (MonadIO m, QueryEvent ev res) => ev -> m res
+liftQuery = liftIO . query
+
+liftUpdate :: (MonadIO m, UpdateEvent ev res) => ev -> m res
+liftUpdate = liftIO . update
