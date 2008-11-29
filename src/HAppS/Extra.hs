1e946f370a6cf1c37f8eee8c28b663bbe8f9821d
David Fox <dsf@seereason.com>
2008-11-29 14:19:05 +0000

    Add HAppS.Server.Main, a main function that does stuff every web application needs to do

--- src/HAppS/Extra.hs
+++ src/HAppS/Extra.hs
@@ -3,6 +3,7 @@ module HAppS.Extra
  , module HAppS.Data.User.Password
  , module HAppS.Server.Account
  , module HAppS.Server.Extra
+ , module HAppS.Server.Main
  , module HAppS.Server.Session
  , module HAppS.State.Extra
  ) where
@@ -11,6 +12,7 @@ import HAppS.Data.IxSet.Extra
 import HAppS.Data.User.Password
 import HAppS.Server.Account
 import HAppS.Server.Extra
+import HAppS.Server.Main
 import HAppS.Server.Session
 import HAppS.State.Extra
 
---
+++ src/HAppS/Server/Main.hs
@@ -0,0 +1,57 @@
+{-# OPTIONS_GHC -Wwarn -fwarn-missing-signatures -fwarn-unused-imports -fwarn-unused-binds #-}
+module HAppS.Server.Main
+    ( main
+    ) where
+
+import Control.Concurrent (MVar, forkIO, killThread)
+import Control.Monad (liftM)
+import HAppS.Server (Conf(..), nullConf, simpleHTTP, wdgHTMLValidator, multi, waitForTermination, ToMessage, ServerPartT)
+import HAppS.Server.CookieFixer(cookieFixer)
+import HAppS.State (Proxy(..), Saver(Queue, FileSaver), createCheckpoint, runTxSystem, shutdownSystem, Methods, Component, TxControl)
+import System.Environment (getArgs)
+import System.Console.GetOpt (OptDescr(..), ArgDescr(..), ArgOrder(..), getOpt)
+
+main :: (ToMessage a, Methods st, Component st) => String -> Proxy st -> (FilePath -> [ServerPartT IO a]) -> IO ()
+main name entryPoint impl = do
+  eConf <- liftM (parseConfig' name) getArgs
+  let conf' =
+          case eConf of
+            Left e -> error (unlines e)
+            Right c -> c
+  control <- startSystemState' (store conf') entryPoint
+  tid <- forkIO $ simpleHTTP (conf conf') [cookieFixer . multi . impl $ static conf']
+  putStrLn ("running on port " ++ show (port (conf conf')) ++ "...")
+  waitForTermination
+  killThread tid
+  createCheckpoint control
+  shutdownSystem control
+
+parseConfig' :: String -> [String] -> Either [String] Conf'
+parseConfig' name args
+    = case getOpt Permute ho args of
+        (flags,_,[]) -> Right $ foldr ($) (standardConf name 8001) flags
+        (_,_,errs)   -> Left errs
+
+ho :: [OptDescr (Conf' -> Conf')]
+ho = [ Option [] ["http-port"] (ReqArg (\h c -> c { conf = (conf c) {port = read h} }) "port") "port to bind http server"
+     , Option [] ["no-validate"] (NoArg (\ c -> c { conf = (conf c) { validator = Nothing } })) "Turn off HTML validation"
+     , Option [] ["validate"] (NoArg (\ c -> c { conf = (conf c) { validator = Just wdgHTMLValidator } })) "Turn on HTML validation"
+     , Option [] ["store"] (ReqArg (\h c -> c {store = h}) "PATH") "The directory used for database storage."
+     , Option [] ["static"] (ReqArg (\h c -> c {static = h}) "PATH") "The directory searched for static files" ]
+
+startSystemState' :: (Component st, Methods st) => String -> Proxy st -> IO (MVar TxControl)
+startSystemState' path proxy =
+    runTxSystem (Queue (FileSaver path)) proxy
+
+data Conf'
+    = Conf' { conf :: Conf
+            , app :: String
+            , store :: FilePath
+            , static :: FilePath }
+
+standardConf :: String -> Int -> Conf'
+standardConf appName defaultPort
+    = Conf' { conf = nullConf {validator = Just wdgHTMLValidator, port = defaultPort}
+            , app = appName
+            , store = "_local/" ++ appName ++ "_state"
+            , static = "static" }
