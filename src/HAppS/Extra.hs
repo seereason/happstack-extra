38c6e0eb0beead2b707052e40752b033c640e8f7
David Fox <dsf@seereason.com>
2008-12-16 13:37:02 +0000

    Export HAppS.Server.SimpleHTTP.Extra

--- src/HAppS/Extra.hs
+++ src/HAppS/Extra.hs
@@ -3,8 +3,10 @@ module HAppS.Extra
  , module HAppS.Data.User.Password
  , module HAppS.Server.Account
  , module HAppS.Server.Extra
+ , import HAppS.Server.HSP.HTML
  , module HAppS.Server.Main
  , module HAppS.Server.Session
+ , module HAppS.Server.SimpleHTTP
  , module HAppS.State.Extra
  ) where
 
@@ -12,7 +14,9 @@ import HAppS.Data.IxSet.Extra
 import HAppS.Data.User.Password
 import HAppS.Server.Account
 import HAppS.Server.Extra
+import HAppS.Server.HSP.HTML
 import HAppS.Server.Main
 import HAppS.Server.Session
+import HAppS.Server.SimpleHTTP
 import HAppS.State.Extra
 
