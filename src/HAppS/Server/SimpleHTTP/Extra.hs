b63406ca63e955f798f1a292164e957e728203a3
David Fox <dsf@seereason.com>
2009-01-25 16:06:56 +0000

    Remove some instances that are now in HAppS-Server

--- src/HAppS/Server/SimpleHTTP/Extra.hs
+++ src/HAppS/Server/SimpleHTTP/Extra.hs
@@ -12,21 +12,6 @@ import HSP.HTML
 import HSP.Identity
 import qualified HSX.XMLGenerator as HSX
 
--- * Move to SimpleHTTP
-
-instance Functor Result where
-    fmap _ NoHandle = NoHandle
-    fmap f (Ok g a) = Ok g (f a)
-    fmap _ (Escape response) = Escape response
-
-instance (Functor m) => Functor (WebT m) where
-    fmap f (WebT m) = WebT (fmap (fmap f) m)
-
-instance (Functor m, Monad m) => Applicative (WebT m) where
-    pure = return
-    (<*>) = ap
-
-
 instance ToMessage (XMLGenT Identity XML) where
     toContentType _ = P.pack "text/html"
     toMessage xml   = toMessage (html4Strict, evalIdentity xml) -- L.fromString (renderAsHTML xml)
