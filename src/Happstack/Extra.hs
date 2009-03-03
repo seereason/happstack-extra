2509be2e5f9eaac6de5ac98372c86793e4d0d616
Clifford Beshers <beshers@seereason.com>
2009-03-03 01:35:55 +0000

    Removed Happstack.Server.HSP.HTML, it has moved upstream.

--- src/Happstack/Extra.hs
+++ src/Happstack/Extra.hs
@@ -3,7 +3,6 @@ module Happstack.Extra
  , module Happstack.Data.User.Password
  , module Happstack.Server.Account
  , module Happstack.Server.Extra
- , module Happstack.Server.HSP.HTML
  , module Happstack.Server.Main
  , module Happstack.Server.Session
  , module Happstack.Server.SimpleHTTP
@@ -14,7 +13,6 @@ import Happstack.Data.IxSet.Extra
 import Happstack.Data.User.Password
 import Happstack.Server.Account
 import Happstack.Server.Extra
-import Happstack.Server.HSP.HTML
 import Happstack.Server.Main
 import Happstack.Server.Session
 import Happstack.Server.SimpleHTTP
