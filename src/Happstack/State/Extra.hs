4e20fd2dba76c256c7798c36cdcc3b4e53f591f2
Jeremy Shaw <jeremy@seereason.com>
2009-03-31 18:15:09 +0000

    remove liftQuery and liftUpdate,because the normal query and update already use liftIO

--- src/Happstack/State/Extra.hs
+++ src/Happstack/State/Extra.hs
@@ -10,12 +10,3 @@ withSystemState :: (Component st, Methods st) => Proxy st -> IO a -> IO a
 withSystemState entryPoint action =
     do control <- startSystemState entryPoint
        action `finally` shutdownSystem control
-
-
--- * these need better names
-
-liftQuery :: (MonadIO m, QueryEvent ev res) => ev -> m res
-liftQuery = liftIO . query
-
-liftUpdate :: (MonadIO m, UpdateEvent ev res) => ev -> m res
-liftUpdate = liftIO . update
