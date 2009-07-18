04a8c966456ceb0b47a7c717b1ae03b90e8d71a9
Jeremy Shaw <jeremy@seereason.com>
2009-07-18 20:50:29 +0000

    added formletPart to Happstack.Server.Formlets

--- src/Happstack/Server/Formlets.hs
+++ src/Happstack/Server/Formlets.hs
@@ -1,17 +1,19 @@
 {-# LANGUAGE FlexibleContexts, FlexibleInstances, PatternGuards, RankNTypes, ScopedTypeVariables #-}
 {-# OPTIONS -Wall -fwarn-missing-signatures -fno-warn-name-shadowing -fwarn-unused-imports #-}
+{-# OPTIONS_GHC -F -pgmFtrhsx #-}
 module Happstack.Server.Formlets
     ( createForm
+    , formletPart
     , handleForm
     , withDatumSP
     ) where
 
-import Control.Applicative.Error (Failing(..))
+import Control.Applicative.Error (Failing(..), ErrorMsg)
 import Control.Arrow (second)
-import Control.Monad (msum)
-import Control.Monad.Trans (MonadIO, lift)
-import Happstack.Server (methodSP, methodM, Method(GET, POST), ok, toResponse, withDataFn,
-                         WebT, Response, ServerPartT(..), anyRequest, notFound, RqData)
+import Control.Monad (MonadPlus,msum)
+import Control.Monad.Trans (MonadIO(liftIO), MonadTrans(lift))
+import Happstack.Server as Happstack -- (methodSP, methodM, Method(GET, POST), ok, toResponse, withDataFn,
+                         -- WebT, Response, ServerPartT(..), anyRequest, notFound, RqData)
 import Happstack.Server.Extra (lookPairsUnicode)
 import Happstack.Server.SimpleHTTP.Extra ()
 import Happstack.State (QueryEvent, query)
@@ -19,6 +21,8 @@ import Data.Generics.SYB.WithClass.Instances ()
 import Extra.URI (URI)
 import Prelude hiding (div, maybe)
 import Text.Formlets (Form, Env, runFormState)
+import HSP
+import qualified HSX.XMLGenerator as HSX
 
 -- |handleForm - a convenience function for using Formlets in Happstack
 handleForm :: forall a. forall xml1. forall xml2. forall m. forall r. (Monad m) =>
@@ -60,3 +64,60 @@ withDatumSP look queryEv f =
            case mResult of
              Nothing -> anyRequest (notFound (toResponse ("Invalid argument: " ++ show x)))
              Just result -> msum (f result)
+
+-- ^ turn a formlet into XML+ServerPartT which can be embedded in a larger document
+formletPart ::
+  (EmbedAsChild m xml, EmbedAsAttr m (Attr String String), MonadIO m, Functor m, ToMessage b, FilterMonad Response m, WebMonad Response m, MonadPlus m, ServerMonad m) 
+  => String -- ^ prefix used to ensure field names are unique
+  -> String -- ^ url to POST form results to
+  -> (a -> XMLGenT m b) -- ^ handler used when form validates
+  -> ([ErrorMsg] -> [XMLGenT m (HSX.XML m)] -> XMLGenT m b) -- ^ handler used when form does not validate
+  -> Form xml IO a -- ^ the formlet
+  -> XMLGenT m (HSX.XML m)
+formletPart prefix action handleSuccess handleFailure form = 
+        withDataFn lookPairsUnicode $ \env ->
+            let (collector, formXML,_) = runFormState (map (second Left) env) prefix form
+            in 
+                 msum [ methodSP POST $ XMLGenT $ Happstack.escape . fmap toResponse $ unXMLGenT $ 
+                          do res <- liftIO collector
+                             case res of
+                               (Success a)      -> handleSuccess a
+                               (Failure faults) -> handleFailure faults [ <form action=action method="POST" enctype="multipart/form-data;charset=UTF-8" accept-charset="UTF-8" >
+                                                                            <% formXML %>
+                                                                          </form> ]
+
+                      , <form action=action method="POST" enctype="multipart/form-data;charset=UTF-8" accept-charset="UTF-8" >
+                         <% formXML %>
+                        </form>
+                      ]
+{-
+testPart :: ServerPartT IO XML
+testPart =
+    do Happstack.escape (ok $ toResponse "foo")
+
+fooPart :: ServerPartT IO XML
+fooPart =
+    unXMLGenT $ 
+    <html>
+     <head>
+      <title>test page</title>
+     </head>
+     <body>
+       <p>foo</p>
+       <% XMLGenT barPart %>
+     </body>
+    </html>
+
+barPart :: ServerPartT IO XML
+barPart =
+    Happstack.escape $ fmap toResponse $ unXMLGenT $ 
+    <html>
+     <head>
+      <title>test page</title>
+     </head>
+     <body>
+       <p>bar</p>
+     </body>
+    </html>
+
+-}
