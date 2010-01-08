1bbc4f01027dfddd7293cc48c522cb66d0e72fe0
Jeremy Shaw <jeremy@seereason.com>
2010-01-08 16:04:03 +0000

    patched to work with formlets remove-xml-monad branch

--- src/Happstack/Server/Formlets.hs
+++ src/Happstack/Server/Formlets.hs
@@ -30,32 +30,31 @@ import qualified HSX.XMLGenerator as HSX
 
 -- |handleForm - a convenience function for using Formlets in Happstack
 handleForm :: forall a. forall xml1. forall xml2. forall m. forall r. (Monad m) =>
-              String -- ^ prefix
-           -> URI -- ^ url for action attribute in form
+              URI -- ^ url for action attribute in form
            -> (xml2 -> r) -- ^ function to render the page that the form goes in
            -> Form xml1 m a -- ^ the form
            -> (a -> WebT m r) -- ^ function which handles POSTed results that successfully validate
            -> (Env -> URI -> [String] -> xml1 -> xml2)
            -> ServerPartT m r
-handleForm prefix action page frm handleOk formXML = msum
-    [ methodM GET >> ok (page (createForm [] prefix action [] frm formXML))
+handleForm action page frm handleOk formXML = msum
+    [ methodM GET >> ok (page (createForm [] action [] frm formXML))
     , withDataFn lookPairs $ \env' ->
         methodSP POST $ anyRequest $
                  do let env = map (second Left) env'
-                        (extractor, _, _) = runFormState env prefix frm                        
+                        (extractor, _, _) = runFormState env frm                        
                     res <- lift extractor
                     case res of
                       Failure faults ->
-                          ok (page (createForm env prefix action faults frm formXML))
+                          ok (page (createForm env action faults frm formXML))
                       Success s -> handleOk s
     ]
 
-createForm :: forall a. forall xml1. forall xml2. forall m. (Monad m) => Env -> String -> URI -> [String] -> Form xml1 m a -> (Env -> URI -> [String] -> xml1 -> xml2) -> xml2
-createForm env prefix action' faults frm formXML =
+createForm :: forall a. forall xml1. forall xml2. forall m. (Monad m) => Env -> URI -> [String] -> Form xml1 m a -> (Env -> URI -> [String] -> xml1 -> xml2) -> xml2
+createForm env action' faults frm formXML =
     -- TheForm env action' faults xml 
     formXML env action' faults xml
     where
-      (_extractor, xml, _ct) = runFormState env prefix frm
+      (_extractor, xml, _ct) = runFormState env frm
 
 -- |Try to extract a datum from the request data and pass it to some
 -- server parts.  This is a generic Happstack function, not formlet specific,
@@ -72,15 +71,14 @@ withDatumSP look queryEv f =
 -- ^ turn a formlet into XML+ServerPartT which can be embedded in a larger document
 formletPart ::
   (EmbedAsChild m xml, EmbedAsAttr m (Attr String String), Functor m, MonadIO m, ToMessage b, FilterMonad Response m, WebMonad Response m, MonadPlus m, ServerMonad m) 
-  => String -- ^ prefix used to ensure field names are unique
-  -> String -- ^ url to POST form results to
+  => String -- ^ url to POST form results to
   -> (a -> XMLGenT m b) -- ^ handler used when form validates
   -> ([ErrorMsg] -> [XMLGenT m (HSX.XML m)] -> XMLGenT m b) -- ^ handler used when form does not validate
   -> Form xml IO a -- ^ the formlet
   -> XMLGenT m (HSX.XML m)
-formletPart prefix action handleSuccess handleFailure form = 
+formletPart action handleSuccess handleFailure form = 
         withDataFn lookEnv $ \env ->
-            let (collector, formXML,_) = runFormState env prefix form
+            let (collector, formXML,_) = runFormState env form
             in 
                  msum [ methodSP POST $ XMLGenT $ Happstack.escape . fmap toResponse $ unXMLGenT $ 
                           do res <- liftIO collector
