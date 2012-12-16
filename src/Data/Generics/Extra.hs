e5367f610b92558a77e77a4c0d2c90829d6331b3
David Fox <dsf@seereason.com>
2012-12-16 21:36:37 +0000

    Add gFind', like gFind but throws an exception if nothing found

--- src/Data/Generics/Extra.hs
+++ src/Data/Generics/Extra.hs
@@ -1,7 +1,8 @@
-module Data.Generics.Extra (gFind) where
+module Data.Generics.Extra (gFind, gFind') where
 
 import Control.Monad (MonadPlus, msum)
 import Data.Generics (Data, Typeable, listify)
+import Data.Maybe (fromJust)
 
 -- | @gFind a@ will extract any elements of type @b@ from
 -- @a@'s structure in accordance with the MonadPlus
@@ -9,3 +10,9 @@ import Data.Generics (Data, Typeable, listify)
 -- found while [Foo] will return the list of Foos found.
 gFind :: (MonadPlus m, Data a, Typeable b) => a -> m b
 gFind = msum . map return . listify (const True)
+
+-- | Acts as gFind but will throw an exception if
+-- nothing is found.
+gFind' :: (Data a, Typeable b) => a -> b
+gFind' = fromJust . gFind
+--Monad versions
