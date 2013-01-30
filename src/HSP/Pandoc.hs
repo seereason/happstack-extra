82edc9af55a0bd2178c99b65b9880d3e48416305
David Fox <dsf@seereason.com>
2013-01-30 01:32:58 +0000

    Update for pandoc-0.10

--- src/HSP/Pandoc.hs
+++ src/HSP/Pandoc.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, UndecidableInstances #-}
 module HSP.Pandoc where
 
+import Data.Default (def)
 import HSP
-import Text.Pandoc (Pandoc, defaultWriterOptions, writeHtmlString)
+import Text.Pandoc (Pandoc, writeHtmlString)
 
 instance (EmbedAsChild m XML) => (EmbedAsChild m Pandoc) where
-    asChild pandoc = asChild (cdata $ writeHtmlString defaultWriterOptions pandoc)
+    asChild pandoc = asChild (cdata $ writeHtmlString def pandoc)
